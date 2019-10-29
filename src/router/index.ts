a799f216fae0179a07f224a593f8cb07e6c86a2e
Ami <ami.nakano@outlook.com>
2019-10-29 15:21:30 +0100

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,36 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home
+  }, 
+  {
+    path: "/login",
+    name: "login",
+    component: ()=>import('@/views/Login.vue')
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: ()=>import('@/views/Register.vue')
+  },
+  {
+    path: "/settings",
+    name: "settings",
+    component: ()=>import('@/views/Settings.vue')
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="auth-page">
+  <div class="container page">
+    <div class="row">
+
+      <div class="col-md-6 offset-md-3 col-xs-12">
+        <h1 class="text-xs-center">Sign in</h1>
+        <p class="text-xs-center">
+          <router-link to='/register'>
+            Need an account?
+          </router-link>
+        </p>
+
+        <!-- <ul class="error-messages">
+          <li>That email is already taken</li>
+        </ul> -->
+
+        <form>         
+          <fieldset class="form-group">
+            <input class="form-control form-control-lg" type="text" 
+            v-model="email"
+            placeholder="Email">
+          </fieldset>
+          <fieldset class="form-group">
+            <input class="form-control form-control-lg" type="password"
+            v-model="password"
+            placeholder="Password">
+          </fieldset>
+          <button @click="login()" class="btn btn-lg btn-primary pull-xs-right">
+            Sign in
+          </button>
+        </form>
+      </div>
+
+    </div>
+  </div>
+</div> 
+</template>
+
+<script>
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class extends Vue {
+  email = ''
+  password = ''
+
+  login(){
+    console.log(`login with email =  ${this.email}`)
+  }
+}
+</script>
